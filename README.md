5c3b12c615659348594641bf2096a2843136700a
T-Pizel <atwano@ucsd.edu>
2026-05-04 21:50:49 -0700

    jest test complete

--- README.md
+++ README.md
@@ -6,6 +6,10 @@
 
 **Repository:** https://github.com/T-Pizel/Lab5_Starter
 
-# GitHub Links
+## GitHub Links
 - expose.html: https://T-Pizel.github.io/Lab5_Starter/expose.html
 - explore.html: https://T-Pizel.github.io/Lab5_Starter/explore.html
+
+## Check Your Understanding
+1. No, I wouldn't use a unit test to test the "message" feature because there are multiple features look after such as the UI, the server taking in and receiving it, or the database. The unit tests would be better ran through integration test or end-to-end test that can actually look through the entire message flow and check if it works.
+2. Yes, I would use a unit test to test the "max message length" feature because this is a single feature to look after. The function takes an input and returns the integer for what would be considered the "max message length." There is no external components to look after, so unit tests would be great since it can easily test these features.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,71 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+test('isPhoneNumber returns true if formatted with dashes', () =>{
+  expect(isPhoneNumber('123-456-7890')).toBe(true);
+});
+test('isPhoneNumber returns true if formatted with parentheses', () =>{
+  expect(isPhoneNumber('(123) 456-7890')).toBe(true);
+});
+test('isPhoneNumber returns false if formatted too short', () =>{
+  expect(isPhoneNumber('12345')).toBe(false);
+});
+test('isPhoneNumber returns false if formatted with letters', () =>{
+  expect(isPhoneNumber('123-456-ABCD')).toBe(false);
+});
+
+//email
+test('isEmail returns true if formatted regularly', () =>{
+  expect(isEmail('user@example.com')).toBe(true);
+});
+test('isEmail returns true if formatted with numbers', () =>{
+  expect(isEmail('user123@domain.com')).toBe(true);
+});
+test('isEmail returns false if formatted with no @ symbol', () =>{
+  expect(isEmail('userdomain.com')).toBe(false);
+});
+test('isEmail returns false if formatted extra letters after domain', () =>{
+  expect(isEmail('user@domain.comm')).toBe(false);
+});
+
+//password
+test('isStrongPassword returns true if formatted with in char limit', () =>{
+  expect(isStrongPassword('Password123')).toBe(true);
+});
+test('isStrongPassword returns true if formatted with underscores', () =>{
+  expect(isStrongPassword('Password_123')).toBe(true);
+});
+test('isStrongPassword returns false if formatted too short', () =>{
+  expect(isStrongPassword('pw1')).toBe(false);
+});
+test('isStrongPassword returns false if format starts with a number', () =>{
+  expect(isStrongPassword('1password')).toBe(false);
+});
+
+//date
+test('isDate returns true if formatted XX/XX/XXXX', () =>{
+  expect(isDate('05/04/2026')).toBe(true);
+});
+test('isDate returns true if formatted X/X/XXXX', () =>{
+  expect(isDate('5/4/2026')).toBe(true);
+});
+test('isDate returns false if formatted with periods', () =>{
+  expect(isDate('05.04.2026')).toBe(false);
+});
+test('isDate returns false if formatted with dashes', () =>{
+  expect(isDate('05-04-2026')).toBe(false);
+});
+
+//hex
+test('isHexColor returns true if formatted with # and six characters', () =>{
+  expect(isHexColor('#FFFFFF')).toBe(true);
+});
+test('isHexColor returns true if formatted with # and three characters', () =>{
+  expect(isHexColor('#FFF')).toBe(true);
+});
+test('isHexColor returns false if formatted with 4 characters', () =>{
+  expect(isHexColor('#FFFF')).toBe(false);
+});
+test('isHexColor returns false if formatted with invalid characters', () =>{
+  expect(isHexColor('#FGFFFF')).toBe(false);
+});
